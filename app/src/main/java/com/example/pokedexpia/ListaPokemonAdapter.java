0a2e2e5eb6b75eda867eb3f8fd7d41c16e3dc073
Erick Lucio <erick.adrian.lu@gmail.com>
2020-07-24 19:07:57 -0500

    first commit

---
+++ app/src/main/java/com/example/pokedexpia/ListaPokemonAdapter.java
@@ -0,0 +1,88 @@
+package com.example.pokedexpia;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+import com.example.pokedexpia.models.Pokemon;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+
+public class ListaPokemonAdapter extends RecyclerView.Adapter<ListaPokemonAdapter.ViewHolder> {
+
+    private ArrayList<Pokemon> dataset;
+    private Context context;
+    private Pokemon p;
+
+    public ListaPokemonAdapter(Context context) {
+        this.context = context;
+        dataset = new ArrayList<>();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_pokemon, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Pokemon p = dataset.get(position);
+        holder.nombreTextView.setText(p.getName());
+
+        Glide.with(context)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/" + p.getNumber() + ".png")
+                .into(holder.fotoImageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataset.size();
+    }
+
+    public void adicionarListaPokemon(ArrayList<Pokemon> listaPokemon) {
+        dataset.addAll(listaPokemon);
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private ImageView fotoImageView;
+        private TextView nombreTextView;
+        private CardView tarjetas;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            fotoImageView = (ImageView) itemView.findViewById(R.id.fotoImageView);
+            nombreTextView = (TextView) itemView.findViewById(R.id.nombreTextView);
+            tarjetas = (CardView) itemView.findViewById(R.id.tarjetas);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.tarjetas:
+
+                    String pokemon =  p.getName();
+
+                    Intent i = new Intent(v.getContext(),DetailActivity.class);
+                    v.getContext().startActivity(i);
+                    Snackbar.make(v, pokemon, Snackbar.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    }
+}
